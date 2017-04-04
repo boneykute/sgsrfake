46d218bef3bea1b8a5753e3995dd2d170e77acc2
Nguyen Minh Hieu <hieunp131@gmail.com>
2017-04-04 11:11:49 +0700

    +Add few lib files, ignore some SFS folder

---
+++ src/main/java/com/sgs/game/sgsr/server/extentions/MyExtension.java
@@ -0,0 +1,12 @@
+package com.sgs.game.sgsr.server.extentions;
+
+import com.smartfoxserver.v2.extensions.SFSExtension;
+
+public class MyExtension extends SFSExtension {
+
+	public void init() {
+		// TODO Auto-generated method stub
+		trace("Hello new structure, this is my NEW Extension!");
+	}
+
+}
